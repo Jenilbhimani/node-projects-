9224b55e771d8561fa28837e371b7735da9a1bc2
Jenilbhimani <95120519+Jenilbhimani@users.noreply.github.com>
2022-03-31 14:08:47 +0530

    31/03.2022
    Mongoose connected with MongoDB in NodeJS.

---
+++ data.js
@@ -0,0 +1,27 @@
+// const bcrypt = require("bcryptjs")
+const data = {
+    "users": [
+        {
+            "_id":3,
+            "name": "Ram",
+            "password": 12345,
+            "isAdmin": true,
+            "email": "ram@skillqode.in"
+        },
+        {
+            "_id":4,
+            "name": "shyam",
+            "password": 1234,
+            "isAdmin": true,
+            "email": "shyam@skillqode.in"
+        },
+        {
+            "_id":5,
+            "name": "raj",
+            "password": 4444,
+            "isAdmin": true,
+            "email": "raj@skillqode.in"
+        }
+    ]
+} 
+module.exports = data;
---
+++ usermodel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const userSchama = new mongoose.Schema({
+    "_id":{type : Number, required: true},
+    "name": { type: String, required: true },
+    "password": { type: Number, required: true },
+    "isAdmin": { type: Boolean, required: true },
+    "email": { type: String, required: true }
+}, { timestamps: true });
+
+const user = mongoose.model("user", userSchama);
+module.exports = user;
